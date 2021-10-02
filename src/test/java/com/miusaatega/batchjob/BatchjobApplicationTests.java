b760cdc664dc673687a876374a72241d3757cda7
tibizy <tobisuleodu@ymail.com>
2021-10-02 10:25:51 -0500

    add java project files

---
+++ src/test/java/com/miusaatega/batchjob/BatchjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miusaatega.batchjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
