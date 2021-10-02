30b060b57c5f238abade2c8b6b599a9e4d9a4d03
tibizy <tobisuleodu@ymail.com>
2021-10-02 13:01:54 -0500

    add rest and security

---
+++ src/main/java/com/miusaatega/batchjob/services/BatchJobService.java
@@ -0,0 +1,12 @@
+package com.miusaatega.batchjob.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BatchJobService {
+
+    public void runJob() {
+        // batchJobHandler.run();
+    }
+
+}
